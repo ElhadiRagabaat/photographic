a84c5e7ae7f4ab7c458c1252715d9bc3838a1b2d
elhadis <ahmad23soho@gmail.com>
2021-06-14 01:19:55 +0300

    firebaseconfig

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,22 +1,24 @@
 import firebase from 'firebase/app';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBAzNIRngGiy7uB3WT4QwTgUfOjyb_oimk",
-//     authDomain: "finalproject-45305.firebaseapp.com",
-//     projectId: "finalproject-45305",
-//     storageBucket: "finalproject-45305.appspot.com",
-//     messagingSenderId: "1075296056541",
-//     appId: "1:1075296056541:web:751e3fc17e52fe00cf9734",
-//     measurementId: "G-KP343J5XHP"
-//   };
 const firebaseConfig = {
-  apiKey: "AIzaSyDRYPPiBFxSn97KtLAP8mYNJyraBFL2qE8",
-  authDomain: "photographic-52ad6.firebaseapp.com",
-  projectId: "photographic-52ad6",
-  storageBucket: "photographic-52ad6.appspot.com",
-  messagingSenderId: "315713218116",
-  appId: "1:315713218116:web:1e43c9cba9fdf997977e11"
-};
+    apiKey: "AIzaSyBAzNIRngGiy7uB3WT4QwTgUfOjyb_oimk",
+    authDomain: "finalproject-45305.firebaseapp.com",
+    projectId: "finalproject-45305",
+    storageBucket: "finalproject-45305.appspot.com",
+    messagingSenderId: "1075296056541",
+    appId: "1:1075296056541:web:751e3fc17e52fe00cf9734",
+    measurementId: "G-KP343J5XHP"
+  };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDbBXbniosRt9VYhZCVLU2qerxFGiE0reI",
+//   authDomain: "poto-ae94a.firebaseapp.com",
+//   projectId: "poto-ae94a",
+//   storageBucket: "poto-ae94a.appspot.com",
+//   messagingSenderId: "1046183146242",
+//   appId: "1:1046183146242:web:ae7667a76af3e37b00481b",
+//   measurementId: "G-FF1V0Q3R1C"
+// };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
 
